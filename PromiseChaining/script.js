d64174575cdaf38e3832a1d4d675ae6cc19d98ab
spandin <willstesi@gmail.com>
2022-11-16 11:53:55 +0300

    Update script.js

--- PromiseChaining/script.js
+++ PromiseChaining/script.js
@@ -4,15 +4,13 @@ function getCountryName(name) {
     .then(data => {
         getCountryInfoByName(data[0])
         const neighbor = data[0].borders
-        console.log(neighbor);
         neighbor.map((key) => {
-            console.log(key);
-            console.log(fetch(`https://restcountries.com/v3.1/alpha/${key}`))
+            console.log(`key`, key);
             return fetch(`https://restcountries.com/v3.1/alpha/${key}`)
+            .then(res2 => res2.json())
+            .then(data2 => getCountryInfoByCode(data2[0]))
         })
     })
-    .then(res2 => res2.json())
-    .then(data2 => getCountryInfoByCode(data2[0]))
 }
 
 function getCountryInfoByName(data) {
@@ -22,7 +20,7 @@ function getCountryInfoByName(data) {
                 <h1>${data.name.common} (${data.region})</h1>
                 <h2>Capital: ${data.capital[0]}</h2>
                 <p>
-                Languages: ${[...Object.values(data.languages)]},
+                Languages: ${[...Object.values(data.languages)]}
                 <br> 
                 Population: ${data.population}
                 </p>
@@ -30,19 +28,19 @@ function getCountryInfoByName(data) {
         `
 }
 
-function getCountryInfoByCode(data) {
+function getCountryInfoByCode(data2) {
     document.querySelector('.border').innerHTML += `
-    <div class='${data.name.common}'>
-        <img src="${data.flags.svg}" alt="">
-        <h1>${data.name.common} (${data.region})</h1>
-        <h2>Capital: ${data.capital[0]}</h2>
+    <div class='${data2.name.common}'>
+        <img src="${data2.flags.svg}" alt="">
+        <h1>${data2.name.common} (${data2.region})</h1>
+        <h2>Capital: ${data2.capital[0]}</h2>
         <p>
-        Languages: ${[...Object.values(data.languages)]}, 
+        Languages: ${[...Object.values(data2.languages)]}
         <br> 
-        Population: ${data.population}
+        Population: ${data2.population}
         </p>
     </div>
     `
 }
 
-getCountryName('Belarus')
+getCountryName(prompt('Введите название страны(eng)', 'Belarus'))
